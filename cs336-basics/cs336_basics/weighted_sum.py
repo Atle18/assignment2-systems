3658c1412f80c663b9020176ecc785e8e37e3975
Atle18 <zhangqiuchen0929@gmail.com>
2025-10-31 23:54:51 +0000

    add

--- cs336-basics/cs336_basics/weighted_sum.py
+++ cs336-basics/cs336_basics/weighted_sum.py
@@ -1,15 +1,9 @@
 import torch
-
-def weighted_sum_simple(x, weight):
-    return (weight * x).sum(dim=-1)
-
-x = torch.rand([4, 20])
-w = torch.ones([20])
-print(weighted_sum_simple(x, w))
-
-
 import triton
 import triton.language as tl
+
+def weighted_sum_simple(x, w):
+    return (w * x).sum(dim=-1)
 
 @triton.jit
 def weighted_sum_fwd(
@@ -32,3 +26,79 @@ def weighted_sum_fwd(
         block_shape=(ROWS_TILE_SIZE, D_TILE_SIZE),
         order=(1,0),
     )
+
+    weight_block_ptr = tl.make_block_ptr(
+        weight_ptr,
+        shape=(D,),
+        strides=(weight_stride_dim,),
+        offsets=(0,),
+        block_shape=(D_TILE_SIZE,),
+        order=(0,),
+    )
+
+    output_block_ptr = tl.make_block_ptr(
+        output_ptr,
+        shape=(ROWS,),
+        strides=(output_stried_row,),
+        offsets=(row_tile_idx * ROWS_TILE_SIZE,),
+        block_shape=(ROWS_TILE_SIZE,),
+        order=(0,),
+    )
+
+    output = tl.zeros((ROWS_TILE_SIZE,), dtype=tl.float32)
+
+    for i in range(tl.cdiv(D, D_TILE_SIZE)):
+        row = tl.load(x_block_ptr, boundary_check=(0, 1), padding_option="zero")
+        weight = tl.load(weight_block_ptr, boundary_check=(0,), padding_option="zero")
+
+        output += tl.sum(row * weight[None, :], axis=1)
+
+        x_block_ptr = x_block_ptr.advance((0, D_TILE_SIZE))
+        weight_block_ptr = weight_block_ptr.advance((D_TILE_SIZE,))
+
+    tl.store(output_block_ptr, output, boundary_check=(0,))
+
+
+class WeightedSumFunc(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight):
+        D, output_dims = x.shape[-1], x.shape[:-1]
+
+        input_shape = x.shape
+        # x = rearrange(x, "... d -> (...) d")
+
+        ctx.save_for_backward(x, weight)
+
+        assert len(weight.shape) == 1 and weight.shape[0] == D, "Dimension mismatch"
+        assert x.is_cuda and weight.is_cuda, "Expected CUDA tensors"
+        assert x.is_contiguous(), "Our pointer arithmetic will assume contiguous x"
+
+        ctx.D_TILE_SIZE = triton.next_power_of_2(D)
+        ctx.ROWS_TILE_SIZE = 16
+        ctx.input_shape = input_shape
+
+        y = torch.empty(output_dims, device=x.device)
+
+        n_rows = y.numel()
+        weighted_sum_fwd[(triton.cdiv(n_rows, ctx.ROWS_TILE_SIZE),)](
+            x, weight,
+            y,
+            x.stride(0), x.stride(1),
+            weight.stride(0),
+            y.stride(0),
+            ROWS=n_rows, D=D,
+            ROWS_TILE_SIZE=ctx.ROWS_TILE_SIZE,
+            D_TILE_SIZE=ctx.D_TILE_SIZE,
+        )
+
+        return y.view(input_shape[:-1])
+
+
+def weighted_sum_triton(x, w):
+    return WeightedSumFunc.apply(x, w)
+
+
+x = torch.rand([4, 20], device="cuda")
+w = torch.ones([20], device="cuda")
+print(weighted_sum_simple(x, w))
+print(weighted_sum_triton(x, w))
